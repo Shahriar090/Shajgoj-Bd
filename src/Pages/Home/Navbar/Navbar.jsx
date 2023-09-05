2746d76b170835965dcdbb90369b1d9df2274bf4
Shahriar090 <shahriarh655@gmail.com>
2023-09-05 14:19:12 +0600

    Fix some bugs

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,106 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import LgNavOptions from "./LgNavOptions";
 import { FaPlus, FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import DropDown from "./DropDown";
 
 const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-white  items-center justify-center">
         <div className="navbar-start">
-          <div className="dropdown">
-            <div className="drawer lg:hidden mr-10 z-50">
-              <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-              <div className="drawer-content">
-                {/* Page content here */}
-                <label htmlFor="my-drawer" className="btn  bg-white">
-                  <GiHamburgerMenu></GiHamburgerMenu>
-                </label>
-              </div>
-              <div className="drawer-side">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
-               
-                <ul className="menu p-4 w-80 min-h-full bg-white text-base-content gap-2">
-                <div>
-                  <h1 className="text-center text-lg uppercase">Menu</h1>
-                  <span className="divider"></span>
-                 <div className="flex gap-3 justify-center">
-                 <h1><FaSearch></FaSearch></h1>
-                 <h1><FaUser></FaUser></h1>
-                 </div>
-                </div>
-                  {/* Sidebar content here */}
-                  <li>
-                   <div className="flex justify-between">
-                   <Link>Make Up</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Skin</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Hair</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Personal Care</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Mom & Baby</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Fragrance</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Undergarments</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Combo</Link>
-                   {/* <h1><FaPlus></FaPlus></h1> */}
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Buy 1 Get 1</Link>
-                   {/* <h1><FaPlus></FaPlus></h1> */}
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Clearance Sale</Link>
-                   {/* <h1><FaPlus></FaPlus></h1> */}
-                   </div>
-                  </li>
-                  <li>
-                  <div className="flex justify-between">
-                   <Link>Men</Link>
-                   <h1><FaPlus></FaPlus></h1>
-                   </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+         <DropDown></DropDown>
           {/* Navbar log and search field */}
           <div className="logo-and-search flex flex-col md:flex-row gap-0 ">
             <div className="logo-container flex flex-row mt-0 md:mt-4">
@@ -115,12 +24,13 @@ const Navbar = () => {
                 <h2 className="font-semibold uppercase md:ml-10">Brands</h2>
               </div>
             </div>
-            <div className="search-field">
+            <div className="search-field relative">
               <input
                 type="text"
                 placeholder="search for products & brands"
-                className="input input-bordered w-44  md:w-full mt-5  md:mt-0 md:ml-36 rounded-full border-[#ff3d71] bg-gray-100"
+                className="input input-bordered w-44  md:w-full mt-5  md:mt-0 md:ml-36 rounded-full border-[#ff3d71] bg-gray-100 text-center"
               />
+             <h1 className="absolute left-40 top-1/2 transform -translate-y-1/2 text-gray-400 hidden md:block lg:block"><FaSearch></FaSearch></h1>
             </div>
           </div>
         </div>
--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -4,59 +4,58 @@ import sliderImgOne from "../../../assets/slider-1.png";
 import sliderImgTwo from "../../../assets/slider-2.jpg";
 import sliderImgThree from "../../../assets/slider-3.jpg";
 import sliderImgFour from "../../../assets/slider-3.png";
-import { GoDotFill, } from "react-icons/go";
+import { GoDotFill } from "react-icons/go";
 import { FaFacebookMessenger, FaShoppingBag } from "react-icons/fa";
-import chatImg from '../../../assets/chat.png'
+
 
 const Slider = () => {
   return (
     <div>
-      
-       <div className="fixed right-0 top-72 bg-[#192038] border-2 border-[#ff3d71] rounded-lg z-50">
-       <div className="p-1">
-        <h1 className="text-2xl text-center text-white "><FaShoppingBag></FaShoppingBag></h1>
-        <p className="text-white">0 Items</p>
+      {/* cart */}
+      <div className="fixed right-0 top-72 bg-[#192038] border-2 border-[#ff3d71] rounded-lg z-50">
+        <div className="p-1">
+          <h1 className="text-2xl text-center text-white ">
+            <FaShoppingBag></FaShoppingBag>
+          </h1>
+          <p className="text-white">0 Items</p>
         </div>
         <div className="bg-[#ff3d71] text-white p-1">
-        <p className="text-center ">$0</p>
+          <p className="text-center ">$0</p>
         </div>
-       
-       </div>
-
-       <div className="help-desk fixed right-6 bottom-10 bg-[#ff3d71] rounded-full z-50">
+      </div>
+      {/* Help desk chat */}
+      <div className="help-desk fixed right-6 bottom-10 bg-[#ff3d71] rounded-full z-50">
         <div>
-          <h1 className="text-3xl text-white p-2"><FaFacebookMessenger></FaFacebookMessenger></h1>
+          <h1 className="text-3xl text-white p-2">
+            <FaFacebookMessenger></FaFacebookMessenger>
+          </h1>
         </div>
-       </div>
-      
+      </div>
+
       <div className="carousel">
-        <div id="item1" className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]">
-          <img
-            src={sliderImgOne}
-            alt="Loading"
-            className="w-full"
-          />
+        <div
+          id="item1"
+          className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]"
+        >
+          <img src={sliderImgOne} alt="Loading" className="w-full" />
         </div>
-        <div id="item2" className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]">
-          <img
-            src={sliderImgTwo}
-            alt="Loading"
-            className="w-full"
-          />
+        <div
+          id="item2"
+          className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]"
+        >
+          <img src={sliderImgTwo} alt="Loading" className="w-full" />
         </div>
-        <div id="item3" className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]">
-          <img
-            src={sliderImgThree}
-            alt="Loading"
-            className="w-full"
-          />
+        <div
+          id="item3"
+          className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]"
+        >
+          <img src={sliderImgThree} alt="Loading" className="w-full" />
         </div>
-        <div id="item4" className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]">
-          <img
-            src={sliderImgFour}
-            alt="Loading"
-            className="w-full"
-          />
+        <div
+          id="item4"
+          className="carousel-item w-full h-[40vh] md:h-[75vh] lg:h-[75vh]"
+        >
+          <img src={sliderImgFour} alt="Loading" className="w-full" />
         </div>
       </div>
       <div className="flex justify-center w-full py-2 gap-2">
@@ -64,13 +63,13 @@ const Slider = () => {
           <GoDotFill></GoDotFill>
         </a>
         <a href="#item2" className="text-sm md:text-xl lg:text-xl">
-        <GoDotFill></GoDotFill>
+          <GoDotFill></GoDotFill>
         </a>
         <a href="#item3" className="text-sm md:text-xl lg:text-xl">
-        <GoDotFill></GoDotFill>
+          <GoDotFill></GoDotFill>
         </a>
         <a href="#item4" className="text-sm md:text-xl lg:text-xl">
-        <GoDotFill></GoDotFill>
+          <GoDotFill></GoDotFill>
         </a>
       </div>
     </div>
